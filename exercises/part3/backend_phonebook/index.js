20005dc2f05cf2f134ff87f4de99b7570205a39b
lazysaint-spec <jaimeevalle@gmail.com>
2025-11-10 18:21:30 +0800

    updated app.listen

--- exercises/part3/backend_phonebook/index.js
+++ exercises/part3/backend_phonebook/index.js
@@ -106,5 +106,6 @@ app.post('/api/persons', (request, response) => {
 
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
